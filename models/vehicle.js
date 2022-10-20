970c6df100d465f0e75c8bb4e1f402a488dbf11d
Derek Lewis <dlewis9442@gmail.com>
2022-10-20 15:18:14 -0500

    git ignore and start models

---
+++ models/vehicle.js
@@ -0,0 +1,14 @@
+const { Schema } = require('mongoose')
+
+const Vehicle = new Schema({
+  make: { type: String, required: true },
+  model: { type: String, required: true },
+  nickname: { type: String, required: true },
+  category: { type: String, required: true },
+  engine: { type: String, required: true },
+  horsepower: { type: Number, required: true },
+  torque: { type: Number, required: true },
+  modifications: { type: String, required: true },
+  image: { type: String, required: true },
+  garage_id: { type: Schema.Types.ObjectId, ref: 'Garage' }
+})
