f5c011cecbd2d0f82bc99182c09a3ed0aa7036b2
Derek Lewis <dlewis9442@gmail.com>
2022-10-24 16:35:51 -0500

    new vehicle form update

--- client/src/pages/VehicleForm.js
+++ client/src/pages/VehicleForm.js
@@ -2,8 +2,21 @@ import { useState } from 'react'
 import axios from 'axios'
 
 const newVehicle = (props) => {
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      let response = await axios.post(
+        `http://localhost:3001/newvehicle`,
+        formState
+      )
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <button type="submit">Add Vehicle</button>
     </form>
   )
