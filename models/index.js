c57762539bdce4a97635a87c59077d4480b0634f
Derek Lewis <dlewis9442@gmail.com>
2022-10-28 10:08:44 -0500

    heroku deployment 4

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const { model } = require('mongoose')
-const garageSchema = require('./Garage')
-const vehicleSchema = require('./Vehicle')
+const garageSchema = require('./Garage.js')
+const vehicleSchema = require('./Vehicle.js')
 
 const Garage = model('Garage', garageSchema)
 const Vehicle = model('Vehicle', vehicleSchema)
