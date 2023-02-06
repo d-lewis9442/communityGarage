77fccaa209eadb4fe1bbd08f9a976a23855313bc
Derek Lewis <dlewis9442@gmail.com>
2023-02-06 14:06:37 -0600

    register testing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
-const { User } = require('../models/User')
-const middleware = require('../middleware')
+const User = require('../models/User')
+const middleware = require('../middleware/index')
 
 const Register = async (req, res) => {
   try {
--- middleware/index.js
+++ middleware/index.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
-const APP_SECRET = process.env.APP_SECRET
+const APP_SECRET = `${process.env.APP_SECRET}`
 
 const hashPassword = async (password) => {
   let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
